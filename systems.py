55723fa927aa64bbb936491f318e79ae0eb8354a
AH-Tran <AH_Tran@hotmail.de>
2021-04-13 19:09:05 +0200

    Deleted Preprocessing

--- systems.py
+++ systems.py
@@ -3,11 +3,8 @@ import json
 import jsonlines
 from elasticsearch import Elasticsearch, helpers
 import re
-import tokens_functions as tf
 from google_trans_new import google_translator
 from elasticsearch.client.ingest import IngestClient
-import create_tokens
-import csv
 
 
 def load_json(directory, id_field):
@@ -22,53 +19,16 @@ def load_json(directory, id_field):
                         '_id': obj[id_field],
                         '_source': obj}
 
-def load_dataframe(es, documents_path, index_name, pipeline_name):
-    for f in os.listdir(documents_path):
-        print ("Create tokens of", f)
-        pd_chunk = create_tokens.main(f)
-        print("Dataframe Created")
-        for success, info in helpers.parallel_bulk(es, doc_generator(pd_chunk, index_name),
-                                            index=index_name, pipeline=pipeline_name):
-            #print("Insert into Index Complete")
-            if not success:
-                return 'A document failed: ' + info, 400
-
-def filterKeys(document):
-    use_these_keys = ['DBRECORDID', 'TITLE', 'ABSTRACT', 'LANGUAGE', 'MESH', 'CHEM', 'KEYWORDS', 'TITLE_TOKENZ_GERMAN', 'TITLE_TOKENZ_SCI', 'ABSTRACT_TOKENZ_GERMAN','ABSTRACT_TOKENZ_SCI', 'KEYWORDS_TOKENZ', 'MESH_TOKENZ', 'CHEM_TOKENZ']
-    return {key: document[key] for key in use_these_keys }
-
-def doc_generator(df, index_name):
-    print("doc_generator started")
-    df_iter = df.iterrows()
-    for index, document in df_iter:
-        yield {
-                "_index": index_name,
-                "_id" : f"{document['DBRECORDID']}",
-                "_source": filterKeys(document),
-            }
-
-def load_csv(directory, id_field):
-    for path, subdir, file in os.walk(directory):
-        extensions = tuple([".csv"])
-        files = [f for f in file if f.endswith(extensions)]
-        for f in files:
-            with open(os.path.join(path, f), 'r') as reader:
-                for obj in reader:  
-                    yield {
-                        '_op_type': 'index',
-                        '_id': obj[id_field],
-                        '_source': obj}
-
 def load_settings(settings_path):
     with open(settings_path, encoding='utf-8') as json_file:
         return json.load(json_file)
 
 
-def load_query_settings(query_settings_path, query_tokenized_ori,
+def load_query_settings(query_settings_path, query_raw,
                         query_tokenized_eng, query_tokenized_german):
-    f = open("./query_settings/query_settings.txt", "r+", encoding="utf-8")
+    f = open("./query_settings/lemuren_query_settings.txt", "r+", encoding="utf-8")
     content = f.read()
-    content = content.replace('"query": "query_tokenized_ori",', '"query": "' + query_tokenized_ori + '",')
+    content = content.replace('"query": "query_raw",', '"query": "' + query_raw + '",')
     content = content.replace('"query": "query_tokenized_eng",', '"query": "' + query_tokenized_eng + '",')
     content = content.replace('"query": "query_tokenized_german",', '"query": "' + query_tokenized_german + '",')
     #content = content.replace('"default_operator": "operator",', '"default_operator": "' + operator + '",')
@@ -102,8 +62,8 @@ def clear_prep_data(prep_data_path):
 class Ranker(object):
     def __init__(self):
         self.INDEX = 'idx_lemuren'
-        self.index_settings_path = os.path.join('index_settings', 'test_settings_multilang.json')
-        self.query_settings_path = os.path.join('query_settings', 'query_settings.txt')
+        self.index_settings_path = os.path.join('index_settings', 'lemuren_index_settings.json')
+        self.query_settings_path = os.path.join('query_settings', 'lemuren_query_settings.txt')
         self.pipeline_settings_path = os.path.join("pipeline_settings", "pipeline_settings.json")
 
         self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -115,30 +75,22 @@ class Ranker(object):
         return self.es.info(), 200
 
     def index(self):
-        #concat_filtered.main()
-        #create_tokens.main()
-        #if self.es.indices.exists(self.INDEX):
-        #    self.es.indices.delete(index=self.INDEX)
 
         load_ingest_pipeline_settings(self.es, self.pipeline_settings_path)
         
+        #check if index exists
         if self.es.indices.exists(self.INDEX):
             print ("Index ", self.INDEX, " already exists. Inserting into index now.")
         else:
             self.es.indices.create(index=self.INDEX, body=load_settings(self.index_settings_path))
-
-        load_dataframe(self.es, self.documents_path, self.INDEX, "attachment")
+        
+        #load into elastic index
+        for success, info in helpers.parallel_bulk(self.es, load_json(self.documents_path, 'DBRECORDID'),
+                                                   index=self.INDEX, pipeline ="attachment"):
+            if not success:
+                return 'A document failed: ' + info, 400
+                
         print ("All Documents have been indexed.  >>END<<")
-        '''
-        for filename in os.listdir(self.documents_path):
-            if filename.endswith('.jsonl'):
-                fullpath=os.path.join(self.documents_path, filename)
-                print("Inserting file ->", filename)
-                with open(fullpath, "r", encoding="utf8") as open_file:
-                    reader = jsonlines.Reader(open_file)
-                    #json_docs.append(jsonlines.Reader(open_file))
-                    helpers.bulk(self.es, reader, ignore = 400,index=self.INDEX, raise_on_error=False, stats_only=False)
-        '''
         #clear_prep_data('./prep_data/filtered_documents')
         return 'Index built with ' + ' docs', 200
 
@@ -151,6 +103,7 @@ class Ranker(object):
             #operator = 'OR'  ### Kritisch weil User potentiell mehrere operatoren kombiniren könnte
             #if ' AND ' in query and not ' OR ' in query:  # wahrscheinlich nur query_string besser
             #    operator = 'AND'
+            query_raw = query
             query = query.replace(" AND ", " +++++ ")
             query = query.replace(" OR ", " ||||| ")
 
@@ -164,25 +117,25 @@ class Ranker(object):
             if type(query_de) == list:
                 query_de = query_de[1]
 
-            query_tokenized_german = tf.tokenize_string_german(query_de)
-            query_tokenized_ori = tf.tokenize_string_sci(query)
-            query_tokenized_eng = tf.tokenize_string_sci(query_eng)
+            #query_tokenized_german = tf.tokenize_string_german(query_de)
+            #query_tokenized_ori = tf.tokenize_string_sci(query)
+            #query_tokenized_eng = tf.tokenize_string_sci(query_eng)
 
-            query_tokenized_german = query_tokenized_german.replace("+,+,+,+,+", "AND")
-            query_tokenized_german = query_tokenized_german.replace("|||||", "OR")
+            query_de = query_de.replace("+,+,+,+,+", "AND")
+            query_de = query_de.replace("|||||", "OR")
 
-            query_tokenized_ori = query_tokenized_ori.replace("+,+,+,+,+", "AND")
-            query_tokenized_ori = query_tokenized_ori.replace("|||||", "OR")
+            query_raw = query_raw.replace("+,+,+,+,+", "AND")
+            query_raw = query_raw.replace("|||||", "OR")
 
-            query_tokenized_eng = query_tokenized_eng.replace("+,+,+,+,+", "AND")
-            query_tokenized_eng = query_tokenized_eng.replace("|||||", "OR")
+            query_eng = query_eng.replace("+,+,+,+,+", "AND")
+            query_eng = query_eng.replace("|||||", "OR")
             es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
             result = es.search(index=self.INDEX,
                                from_=start,
                                size=rpp,
-                               body=load_query_settings(self.query_settings_path, query_tokenized_ori,
-                                                        query_tokenized_eng, query_tokenized_german))
+                               body=load_query_settings(self.query_settings_path, query_raw,
+                                                        query_eng, query_de))
 
             for res in result["hits"]["hits"]:
                 try:
